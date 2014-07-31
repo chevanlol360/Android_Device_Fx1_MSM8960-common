160b9d95c922da53176bc994a52c207e73c62529
Chevanlol360 <chevanlol360@CQ-ABA-HP.(none)>
2014-07-30 20:11:23 -0400

    CM11 Cleaning up

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -26,25 +26,32 @@
 
 COMMON_GLOBAL_CFLAGS += -DLGE_MSM8960 -DNEED_UMS_ENABLE -DENABLE_UMS_WITH_DATAMEDIA
 
-# Bootloader
-TARGET_NO_BOOTLOADER := true
 
 # Platform
 TARGET_BOARD_PLATFORM := msm8960
-TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno225
 
 # Architecture
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_SMP := true
-TARGET_USE_KRAIT_BIONIC_OPTIMIZATION := true
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+TARGET_CPU_VARIANT := krait
 ARCH_ARM_HAVE_TLS_REGISTER := true
 
+# Krait optimizations
+TARGET_USE_KRAIT_BIONIC_OPTIMIZATION := true
+TARGET_USE_KRAIT_PLD_SET := true
+TARGET_KRAIT_BIONIC_PLDOFFS := 10
+TARGET_KRAIT_BIONIC_PLDTHRESH := 10
+TARGET_KRAIT_BIONIC_BBTHRESH := 64
+TARGET_KRAIT_BIONIC_PLDSIZE := 64
+
 # Flags
-TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
-COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE
+TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
+COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE -DQCOM_BSP
 
 # Preload bootanimation
 TARGET_BOOTANIMATION_PRELOAD := true
@@ -54,8 +61,6 @@ TARGET_BOOTANIMATION_PRELOAD := true
 # PMEM compatibility
 BOARD_NEEDS_MEMORYHEAPPMEM := true
 
-# Init
-TARGET_PROVIDES_INIT_RC := true
 
 # QCOM hardware
 BOARD_USES_QCOM_HARDWARE := true
@@ -67,61 +72,49 @@ TARGET_USES_ION_AUDIO := true
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
 
-# FM radio
-#BOARD_HAVE_FM_RADIO := true
-#BOARD_GLOBAL_CFAGS += -DHAVE_FM_RADIO
-
 # QCOM GPS
 #BOARD_USES_QCOM_GPS := true
+
+# QCOM PowerHAL
+TARGET_POWERHAL_VARIANT := qcom
+TARGET_USES_CPU_BOOST_HINT := true
 
 # Graphics
 COMMON_GLOBAL_CFLAGS += -DQCOM_NO_SECURE_PLAYBACK -DQCOM_ROTATOR_KERNEL_FORMATS
 USE_OPENGL_RENDERER := true
 TARGET_USES_C2D_COMPOSITION := true
 TARGET_USES_ION := true
-#TARGET_HAVE_BYPASS := false
-#TARGET_USES_OVERLAY := true
-#TARGET_QCOM_HDMI_OUT := true
+TARGET_USES_SF_BYPASS := true
+TARGET_DISPLAY_USE_RETIRE_FENCE := true
 BOARD_EGL_CFG := device/lge/msm8960-common/configs/egl.cfg
+TARGET_QCOM_DISPLAY_VARIANT := caf
 
 # Webkit
 ENABLE_WEBGL := true
 TARGET_FORCE_CPU_UPLOAD := true
 
+TARGET_DISPLAY := QHD
 
-# Flags for Krait CPU
-COMMON_GLOBAL_CFLAGS += -D__ARM_USE_PLD -D__ARM_CACHE_LINE_SIZE=64
-ifneq ($(VARIENT_REQUIRE_3.0_KERNEL),true)
-COMMON_GLOBAL_CFLAGS += -DNEW_ION_API
-endif
-TARGET_CPU_VARIANT := krait
 
-# Platform
-TARGET_BOARD_PLATFORM := msm8960
-TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
-TARGET_ARCH_VARIANT_CPU := cortex-a9
-
-TARGET_QCOM_DISPLAY_VARIANT := caf
-
-#BOARD_SEPOLICY_DIRS += \
-#        device/lge/msm8960-common/sepolicy
+BOARD_SEPOLICY_DIRS += \
+        device/lge/msm8960-common/sepolicy
 		
-#BOARD_SEPOLICY_UNION += \
-#	genfs_contexts \
-#	app.te \
-#	bluetooth.te \
-#	device.te \
-#	domain.te \
-#	drmserver.te \
-#	file.te \
-#	file_contexts \
-#	hci_init.te \
-#	init_hell.te \
-#	keystore.te \
-#	mediaserver.te \
-#	kickstart.te \
-#	rild.te \
-#	surfaceflinger.te \
-#	system.te \
-#	ueventd.te \
-#	wpa.te
+BOARD_SEPOLICY_UNION += \
+	genfs_contexts \
+	app.te \
+	bluetooth.te \
+	device.te \
+	domain.te \
+	drmserver.te \
+	file.te \
+	file_contexts \
+	hci_init.te \
+	init_hell.te \
+	keystore.te \
+	mediaserver.te \
+	kickstart.te \
+	rild.te \
+	surfaceflinger.te \
+	system.te \
+	ueventd.te \
+	wpa.te
